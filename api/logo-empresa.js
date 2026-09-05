ea168b15c988bc52d8b16dbb38b058193a1b030e
infotactacuarembo-dev <infotactacuarembo@gmail.com>
2026-09-05 12:42:47 -0300

    Update logo-empresa.js

--- api/logo-empresa.js
+++ api/logo-empresa.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const { createClient } = require('@supabase/supabase-js');
 const { requireSession, getSessionUser } = require('./_auth');
 
