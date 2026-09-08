597a153ab9c14717ae301447b0861bff71ab3d66
infotactacuarembo-dev <infotactacuarembo@gmail.com>
2026-09-08 15:30:26 -0300

    Update ordenes.js

--- api/ordenes.js
+++ api/ordenes.js
@@ -5,7 +5,7 @@ const ORDER_FIELDS = `
   id, fecha, cliente_id, cliente, tel, tipo, serie, pass,
   sena, falla, presupuesto, presupuesta, estetico,
   diagnostico, trabajo_realizar, aprobacion_presupuesto,
-  estado, fecha_entrega, empresa_id, tecnico_id`;
+  estado, fecha_entrega, empresa_id, tecnico_id, tecnico_nombre`;
 
 const ALLOWED_STATES = new Set([
   'ingresado',
